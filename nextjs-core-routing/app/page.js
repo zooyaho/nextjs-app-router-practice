690fcb0a31e5882d3c70fc87586c3f0c5ab24389
zooyaho <jiwoo1268@naver.com>
2024-09-24 10:34:46 +0900

    * 초기 소스 코드 수정

--- nextjs-core-routing/app/page.js
+++ nextjs-core-routing/app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }

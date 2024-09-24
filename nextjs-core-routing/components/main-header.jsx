bd70b8b6c49531ad0f0401f5e6ad157a331e15a6
zooyaho <jiwoo1268@naver.com>
2024-09-24 12:46:42 +0900

    * tailwind 및 관련 라이브러리 설치, header에 스타일 적용

--- nextjs-core-routing/components/main-header.jsx
+++ nextjs-core-routing/components/main-header.jsx
@@ -1,21 +1,40 @@
 import Link from "next/link";
-import LogoSrc from "@/assets/logo.png";
+import logoImg from "@/assets/logo.png";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href={"/"}>
-        <img src={LogoSrc.src} alt="사이트 로고, 접시 위에 담긴 음식" />
-        NextLevel food
+    <header className="flex justify-between items-center p-8 md:px-[10%]">
+      <Link
+        href="/"
+        className="flex items-center gap-8 text-[#ddd6cb] font-bold uppercase tracking-wide text-2xl"
+      >
+        <Image
+          src={logoImg}
+          alt="사이트 로고, 접시 위에 담긴 음식"
+          className="w-20 h-20 object-contain drop-shadow-lg"
+          priority
+        />
+        NextLevel Food
       </Link>
 
-      <nav>
-        <ul>
+      <nav className="nav">
+        <ul className="flex list-none m-0 p-0 gap-6 text-xl">
           <li>
-            <Link href={"/meals"}>Browse Meals</Link>
+            <Link
+              href="/meals"
+              className="text-[#ddd6cb] font-bold py-2 px-4 rounded-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-[#ff8a05] hover:to-[#f9b331] hover:text-shadow active:bg-clip-text active:text-transparent active:bg-gradient-to-r active:from-[#ff8a05] active:to-[#f9b331]"
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href={"/community"}>Foodies Community</Link>
+            <Link
+              href="/community"
+              className="text-[#ddd6cb] font-bold py-2 px-4 rounded-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-[#ff8a05] hover:to-[#f9b331] hover:text-shadow active:bg-clip-text active:text-transparent active:bg-gradient-to-r active:from-[#ff8a05] active:to-[#f9b331]"
+            >
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
---
+++ nextjs-core-routing/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // 파일들을 대상으로 Tailwind CSS가 적용될 수 있도록 설정
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

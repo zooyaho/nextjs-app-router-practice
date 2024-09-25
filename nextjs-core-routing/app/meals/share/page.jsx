3d26e4140435aa0ad919dbcd8a49b340819cb3b8
zooyaho <jiwoo1268@naver.com>
2024-09-25 15:57:36 +0900

    * 폼 제출 상태 피드백 ui 추가

--- nextjs-core-routing/app/meals/share/page.jsx
+++ nextjs-core-routing/app/meals/share/page.jsx
@@ -1,4 +1,6 @@
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 import { shareMeal } from "@/lib/action";
 
 export default function ShareMealPage() {
@@ -94,12 +96,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label={"your image"} name={"image"} />
           <p className="text-right">
-            <button
-              type="submit"
-              className="py-3 px-8 rounded-md bg-gradient-to-r from-[#f9572a] to-[#ff9b05] text-white font-bold text-lg shadow-md hover:bg-gradient-to-r hover:from-[#fd4715] hover:to-[#f9b241] focus:ring-2 focus:ring-offset-2 focus:ring-[#f99f2a] disabled:bg-[#ccc] disabled:text-[#979797] disabled:cursor-not-allowed"
-            >
-              Share Meal
-            </button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>

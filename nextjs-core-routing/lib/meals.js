809c3951374b845fd5ee54f0bea9d9647a20ee94
zooyaho <jiwoo1268@naver.com>
2024-09-25 11:45:01 +0900

    * meals페이지 에러처리 로직 추가, 404페이지 추가

--- nextjs-core-routing/lib/meals.js
+++ nextjs-core-routing/lib/meals.js
@@ -4,5 +4,6 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000)); // 지연 동작을 시뮬레이션하기 위한 코드
+  // throw new Error("식사 불러오기를 실패 했습니다.");
   return db.prepare("SELECT * FROM meals").all();
 }
